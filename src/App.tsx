7c53765bc3eecd3090d77411c3b077af4d929e43
Jake <Jake@Jakes-Desktop.local>
2025-03-16 11:39:15 -0600

    Finished FCC Tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,64 @@
-const App = () => {
-  return <div>App</div>;
+import axios from 'axios';
+import { FC, useEffect, useState } from 'react';
+import User from './components/User';
+import { AppProps, UserModel } from './App.types';
+
+
+const App: FC<AppProps> = ({ title }) => {
+  const [users, setUsers] = useState<UserModel[]>([]);
+  const [username, setUsername] = useState<String>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     try {
+  //       setIsLoading(true);
+  //       const { data } = await axios.get('https://randomuser.me/api/?results=10');
+  //       console.log('User Data: ', data);
+  //       setUsers(data.results);
+  //     } catch (err) {
+  //       console.error('Error getting users: ', err);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   getUsers();
+  // }, []);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get('https://randomuser.me/api/?results=10');
+      console.log('Getting User Data As Requested: ', data);
+      setUsers(data.results);
+    } catch (err) {
+      console.error('Error Fetching Users: ', err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button onClick={handleClick}>Show Users</button>
+      <input type='text' onChange={handleChange} />
+      <div>{username}</div>
+      {isLoading && <p>...Loading</p>}
+      <ul>
+        {users.map(({ login, name, email }) => {
+          return (
+            <User key={login.uuid} name={name} email={email} />
+          )
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/App.types.ts
@@ -0,0 +1,24 @@
+export interface AppProps {
+    title: string;
+  }
+  
+  export interface NameModel {
+    first: string;
+    last: string;
+  }
+  
+  export interface LoginModel {
+    uuid: string;
+  }
+  
+  export interface UserModel {
+    name: NameModel;
+    login: LoginModel;
+    email: string;
+  }
+
+  export interface UserProps {
+    name: NameModel;
+    email: string;
+  }
+  
